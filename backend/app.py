5818086a43b2fe5380329f3898933fd2a9d8b82e
Gokul Rathod <g2rindian2005@gmail.com>
2026-06-25 12:56:58 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -24,7 +24,7 @@ app.config.update(
     MAIL_PORT=587,
     MAIL_USE_TLS=True,
     MAIL_USERNAME='gokul9356782322@gmail.com', 
-    MAIL_PASSWORD='tuewvjbrolzdllyh' 
+    MAIL_PASSWORD='tuew vjbr olzd llyh' 
 )
 mail = Mail(app)
 
